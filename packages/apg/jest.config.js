8d31eb023b7329f0d386980126cd72f5fad67483
Joel Gustafson <joelg@mit.edu>
2021-03-13 22:19:54 -0500

    Update 17 dependencies
    @babel/cli
      * @underlay/apg-format-rdf: ^7.13.10 → 7.13.10
    
    @babel/plugin-transform-modules-commonjs
      * @underlay/apg: ^7.12.13 → 7.13.8
      * @underlay/apg-format-binary: ^7.12.13 → 7.13.8
      * @underlay/apg-format-rdf: ^7.12.13 → 7.13.8
      * @underlay/apg-schema-schema: ^7.12.13 → 7.13.8
    
    @babel/preset-typescript
      * @underlay/apg: ^7.12.16 → 7.13.0
      * @underlay/apg-format-binary: ^7.12.16 → 7.13.0
      * @underlay/apg-format-rdf: ^7.12.16 → 7.13.0
      * @underlay/apg-schema-schema: ^7.12.16 → 7.13.0
    
    @babel/core
      * @underlay/apg: ^7.13.10 → 7.13.10
      * @underlay/apg-format-binary: ^7.13.10 → 7.13.10
      * @underlay/apg-format-rdf: ^7.13.10 → 7.13.10
      * @underlay/apg-schema-schema: ^7.13.10 → 7.13.10
    
    @types/jest
      * @underlay/apg: ^26.0.20 → 26.0.20
      * @underlay/apg-format-binary: ^26.0.20 → 26.0.20
      * @underlay/apg-format-rdf: ^26.0.20 → 26.0.20
      * @underlay/apg-schema-schema: ^26.0.20 → 26.0.20
    
    @types/rdf-js
      * @underlay/apg-format-rdf: ^4.0.1 → 4.0.1
    
    @types/uuid
      * @underlay/apg-format-rdf: ^8.3.0 → 8.3.0
    
    @types/varint
      * @underlay/apg-format-binary: ^5.0.0 → 6.0.0
    
    @types/yargs
      * @underlay/apg-cli: ^16.0.0 → 16.0.0
    
    @underlay/namespaces
      * @underlay/apg: ^0.0.3 → 0.0.3
      * @underlay/apg-codec-relational: ^0.0.3 → 0.0.3
      * @underlay/apg-codec-table: ^0.0.3 → 0.0.3
      * @underlay/apg-format-binary: ^0.0.3 → 0.0.3
      * @underlay/apg-format-rdf: ^0.0.3 → 0.0.3
      * @underlay/apg-schema-mapping: ^0.0.3 → 0.0.3
      * @underlay/apg-schema-schema: ^0.0.3 → 0.0.3
    
    bignumber.js
      * @underlay/apg-format-binary: ^9.0.1 → 9.0.1
    
    babel-jest
      * @underlay/apg: ^26.6.3 → 26.6.3
      * @underlay/apg-format-binary: ^26.6.3 → 26.6.3
      * @underlay/apg-schema-schema: ^26.6.3 → 26.6.3
    
    cbor
      * @underlay/apg-format-binary: ^7.0.1 → 7.0.3
    
    io-ts
      * @underlay/apg-cli: ^2.2.13 → 2.2.16, ^2.2.13 → 2.2.16
      * @underlay/apg-codec-apg: ^2.2.13 → 2.2.16, ^2.2.13 → 2.2.16
      * @underlay/apg-codec-relational: ^2.2.13 → 2.2.16, ^2.2.13 → 2.2.16
      * @underlay/apg-codec-table: ^2.2.13 → 2.2.16, ^2.2.13 → 2.2.16
    
    fp-ts
      * @underlay/apg-cli: 2.9.5, 2.9.5
      * @underlay/apg-codec-apg: 2.9.5, 2.9.5
      * @underlay/apg-codec-relational: 2.9.5, 2.9.5
      * @underlay/apg-codec-table: 2.9.5, 2.9.5
      * @underlay/apg-format-rdf: 2.9.5, 2.9.5
    
    typescript
      * @underlay/apg: ^4.1.5 → 4.2.3
      * @underlay/apg-cli: ^4.1.5 → 4.2.3
      * @underlay/apg-codec-apg: ^4.1.5 → 4.2.3
      * @underlay/apg-codec-relational: ^4.1.5 → 4.2.3
      * @underlay/apg-codec-table: ^4.1.5 → 4.2.3
      * @underlay/apg-format-binary: ^4.1.5 → 4.2.3
      * @underlay/apg-format-rdf: ^4.1.5 → 4.2.3
      * @underlay/apg-schema-mapping: ^4.1.5 → 4.2.3
      * @underlay/apg-schema-schema: ^4.1.5 → 4.2.3
    
    signed-varint
      * @underlay/apg-format-binary: ^2.0.1 → 2.0.1

--- packages/apg/jest.config.js
+++ packages/apg/jest.config.js
@@ -1,13 +1,6 @@
 export default {
 	testEnvironment: "node",
-	transformIgnorePatterns: ["node_modules/(?!(@underlay/namespaces)/)"],
 	transform: {
-		"node_modules/@underlay/namespaces/*\\.js$": [
-			"babel-jest",
-			{
-				plugins: ["@babel/plugin-proposal-export-namespace-from"],
-			},
-		],
 		"\\.(j|t)s$": [
 			"babel-jest",
 			{
